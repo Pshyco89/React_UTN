f0809aea86cc84fc8dfdcdb2677436506f4c54cb
Ronald Fernando Rodriguez Morocho <rrodriguezmorocho@softtek.com>
2025-07-30 20:22:58 -0300

    Implementación de AuthProvider en main general

--- tp_module_final/src/main.jsx
+++ tp_module_final/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RoutesComponent } from './Router/RoutesComponent/RoutesComponent';
+import { AuthProvider } from "./context/AuthContext";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RoutesComponent />
+        <AuthProvider>
+            <RoutesComponent />
+        </AuthProvider>
     </React.StrictMode>
 );
-
