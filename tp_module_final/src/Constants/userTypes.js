acf10e6bc4822e4c98bb83b27f715eca335fdaa9
Ronald Fernando Rodriguez Morocho <rrodriguezmorocho@softtek.com>
2025-08-01 15:50:08 -0300

    Constantes globales

--- tp_module_final/src/Constants/userTypes.js
+++ tp_module_final/src/Constants/userTypes.js
@@ -2,3 +2,7 @@ export const userId = 1;
 export const userDescription = "Usuario";
 export const adminId = 2;
 export const adminDescription = "Administrador";
+export const userEditorId = 3;
+export const userEditorDescription = "UserEditor";
+export const adminEditorId = 4;
+export const adminEditorDescription = "AdminEditor";
